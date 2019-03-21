d064debbe7e426771c83281239e6b817d88d015f
ChocW <48013847+ChocW@users.noreply.github.com>
2019-03-21 15:36:20 +0100

    Add files via upload

---
+++ fonction.php
@@ -0,0 +1,19 @@
+<?php
+
+$nomFilmVO = 'Indiana Jones and the Last Crusade';
+echo $nomFilmVO;
+echo '<br/>';
+
+$filmVuOuNon = 'NON';
+echo $filmVuOuNon;
+echo '<br/>';
+
+$annéesortiesalle = '18 October 1989';
+echo $annéesortiesalle;
+echo '<br/>';
+
+$noteFilmIMDB = '8.2/10';
+echo $noteFilmIMDB;
+
+
+?>
